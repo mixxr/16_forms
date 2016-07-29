e3eaf61ac6bd557782ecfb47ceb2a1c647957769
mixxr <ms.co.it@gmail.com>
2016-07-29 10:10:36 +0200

    form with insert/edit

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,9 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {  provideForms } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  provideForms()
+ ])
+ .catch((err: any) => console.error(err));
